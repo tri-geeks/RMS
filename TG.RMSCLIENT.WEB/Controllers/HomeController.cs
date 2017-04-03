373621c845063c940b1b3684cf1c689de484f4de
Moinul Islam <moinulmithu@hotmail.com>
2017-04-03 16:54:29 +0600

    Rating model added

--- TG.RMSCLIENT.WEB/Controllers/HomeController.cs
+++ TG.RMSCLIENT.WEB/Controllers/HomeController.cs
@@ -15,5 +15,29 @@ namespace TG.RMSCLIENT.WEB.Controllers
         {
             return View();
         }
+        public JsonResult SendRating(string r, string s, string id, string url)
+        {
+            int autoId = 0;
+            int thisRating = 0;
+            int sectionId = 0;
+            int.TryParse(s, out sectionId);
+            int.TryParse(r, out thisRating);
+            int.TryParse(id, out autoId);
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Json("Not authenticated!");
+            }
+            if (autoId.Equals(0))
+            {
+                return Json("Record to vote doesn't exist");
+            }
+            switch (s)
+            {
+                case "5":
+                    var isIt = 
+                default:
+                    break;
+            }
+        }
     }
 }

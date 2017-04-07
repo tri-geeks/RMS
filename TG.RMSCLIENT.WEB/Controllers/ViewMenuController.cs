f54e9a9f2ec7901a521919ca9f7c4f89f3c45880
smaatique <smaatique@gamil.com>
2017-04-08 00:59:27 +0600

    added new controller ViewMenuController

---
+++ TG.RMSCLIENT.WEB/Controllers/ViewMenuController.cs
@@ -0,0 +1,27 @@
+﻿using BGW.MANAGER.FoodChartMenuManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TG.RMSCLIENT.WEB.Controllers
+{
+    public class ViewMenuController : Controller
+    {
+        //
+        // GET: /ViewMenu/
+        FoodChartMenuManager _foodChartMenuManager = new FoodChartMenuManager();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult LoadFoodMenu()
+        {
+            var list = _foodChartMenuManager.LoadTabItem();
+            return Json(_foodChartMenuManager.LoadTabItem(), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
